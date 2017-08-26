9cbfaf29c835ebc57cef3c5e0563aaf16eb4b143
Boyi Li <boyizjuer2017@gmail.com>
2017-08-27 00:47:04 +0800

    add eucli_distance

--- knn_kdtree/myutil.py
+++ knn_kdtree/myutil.py
@@ -1,4 +1,20 @@
 def get_median(data):
+	"""input: 
+	   		list : one-dimension feature
+	   output: 
+	   		int : middle value
+	   ---------
+	"""
 	data.sort()
 	half = len(data) // 2
 	return data[half]
+
+def eucli_distance(point_1, point_2):
+	"""input: 
+	   		list : point_1, point_2
+	   output: 
+	   		int : middle value
+	   ---------
+	"""
+	
+	
