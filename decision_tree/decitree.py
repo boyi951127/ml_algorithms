856a1fb982941621c957b5392bc905e851af6268
Boyi Li <boyizjuer2017@gmail.com>
2017-09-15 00:54:44 +0800

    decision tree

---
+++ decision_tree/decitree.py
@@ -0,0 +1,15 @@
+class DecisionTree:
+
+	def read_in_data:
+
+	def cal_info_gain:
+
+	def cal_info_gain_ratio:
+
+	def ID3_create_tree:
+
+	def C45_create_tree:
+
+	def CART_create_tree:
+
+	def print_tree:
